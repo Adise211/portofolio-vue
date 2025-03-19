79c48d47debfa476b689f030a7ed71109703fd04
Adise <adise@wyze.co.il>
2025-03-19 18:46:37 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <RouterView />
+</template>
+
+<script>
+import { RouterView } from 'vue-router'
+
+export default {
+  name: 'App',
+  components: { RouterView },
+  props: {},
+  data: () => ({}),
+  created() {},
+  mounted() {},
+  methods: {},
+  computed: {},
+  watch: {},
+}
+</script>
+
+<style scoped></style>
---
+++ src/views/MainPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>New Component</div>
+</template>
+
+<script>
+export default {
+  name: 'MainPage',
+  components: {},
+  props: {},
+  data: () => ({}),
+  created() {},
+  mounted() {},
+  methods: {},
+  computed: {},
+  watch: {},
+}
+</script>
+
+<style scoped></style>
